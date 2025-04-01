d262f0887fd8ea15554e6874437160949c50fc4b
mintuchel <max3627@naver.com>
2025-04-01 17:26:38 +0900

    fix: dependency for aws s3

--- src/main/java/ensharp/pinterest/PinterestApplication.java
+++ src/main/java/ensharp/pinterest/PinterestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PinterestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PinterestApplication.class, args);
 	}
-
 }
